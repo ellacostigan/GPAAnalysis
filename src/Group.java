0d5cb66fd5d3861032faca2b0da5b1dce35da27c
ellacostigan <70350987+ellacostigan@users.noreply.github.com>
2021-10-28 16:26:22 -0400

    commit

--- src/Group.java
+++ src/Group.java
@@ -1,6 +1,19 @@
 //group
 public class Group{
+public void group_array(){
 
+}
+public void group_gpa(){
 
+}
+public void group_name(){
+
+}
+public void compare_section_to_group(){
+
+}
+public void compare_group(){
+    
+}
 
 }
