30467c8a4f122b20224674733c4dcc5f10a51057
ellacostigan <70350987+ellacostigan@users.noreply.github.com>
2021-10-28 16:22:37 -0400

    enter

--- src/Section.java
+++ src/Section.java
@@ -1,1 +1,33 @@
 //section
+public class Section(){
+
+    public void student_name_array(){
+
+    }
+
+    public void student_id_array(){
+
+    }
+    public void student_gpa_array(){
+
+    }
+    public void section_name(){
+
+    }
+    public void section_professor(){
+        
+    }
+    public void section_average(){
+
+    }
+    public void build_student_arrays(){
+
+    }
+    public void calculate_average(){
+
+    }
+    public void compare_section(){
+        
+    }
+
+}
