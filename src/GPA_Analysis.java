3c80142b1abb75252bfec28fb56e865423aa7222
ellacostigan <70350987+ellacostigan@users.noreply.github.com>
2021-10-28 20:20:02 -0400

    commit

--- src/GPA_Analysis.java
+++ src/GPA_Analysis.java
@@ -3,15 +3,17 @@ import java.io.*;
 
 public class GPA_Analysis {
     public static void main(String[]args){
-        //Code 
+       
     }
 
-    public void import_files() throws FileNotFoundException{
+    public static void import_files() throws FileNotFoundException{
         File file = new File("AllGroups.txt");
         Scanner sc = new Scanner(file);
         
         while (sc.hasNextLine())
             System.out.println(sc.nextLine());
+
+        
     }
     public void create_report(){
     
