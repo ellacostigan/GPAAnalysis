0491ffb0c90d65509f00f18720b3725446a54c29
ssturgis23 <ssturgis646@g.rwu.edu>
2021-11-16 00:24:12 -0500

    major changes

--- src/Group.java
+++ src/Group.java
@@ -1,14 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //group
 public class Group{
-public Section group_of_sections_array[];
-public float group_gpa;
-public String group_name;
+    public Section group_of_sections_array[];
+    public float group_gpa;
+    public String group_name;
 
-public void compare_section_to_group(){
+    public Group(String groups, Section[] sections) throws FileNotFoundException {
+        File tempFile = new File("src/" + groups);
+        Scanner scan = new Scanner(tempFile);
+        this.group_name = scan.nextLine();
+        String tempLine = "";
+        while (scan.hasNextLine()){
+            tempLine = scan.nextLine();
+            // Organizing groups
+        }
+    }
 
-}
-public void compare_group(){
-    
-}
+    public void compare_section_to_group(){
+
+    }
+    public void compare_group(){
+        
+    }
 
 }
